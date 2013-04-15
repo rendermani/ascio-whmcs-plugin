f83fd72aafd2e0d660b6180102738648d365e953
Manuel Lautenschlager <manuel.lautenschlager@ascio.com>
2013-04-15 11:51:57 +0200

    Cleanup hooks.php

--- hooks.php
+++ hooks.php
@@ -2,13 +2,7 @@
  function hook_set_domain_status($vars) {  
 
  	// what is in $vars?	
-	
  	//this should work. it works when I call the php directly.
- 	$debug = print_r($vars,true);
-
-	$fp = fopen("/home/webmaste/public_html/clients/modules/registrars/ascio/debug_manuel_vars.txt", "w");
-	fwrite($fp,$debug);
-	fclose($fp);
 
 	// here I try to set the data to Pending
     $domain = $vars["sld"] ."." . $vars["tld"];
