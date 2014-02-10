19930465f45b6870b87ae81df01aec4a3135ea8c
apocas <petermdias@gmail.com>
2014-02-10 17:28:20 +0000

    cleaning

--- ascio.php
+++ ascio.php
@@ -15,13 +15,10 @@
 require("ascio-lib.php");
 
 function ascio_getConfigArray() {
-	$configarray = array(
-	 "Username" => array( "Type" => "text", "Size" => "20", "Description" => "Enter your username here", ),
-	 "Password" => array( "Type" => "password", "Size" => "20", "Description" => "Enter your password here", ),
-	 "TestMode" => array( "Type" => "yesno", ),
-	);
+	$configarray = array();
 	return $configarray;
 }
+
 function ascio_GetNameservers($params) {
 	$domain = searchDomain($params); 
 	if (is_array($domain)) return $domain;
@@ -29,10 +26,11 @@ function ascio_GetNameservers($params) {
 	# Put your code to get the nameservers here and return the values below
 	$values["ns1"] = $ns->NameServer1->HostName;
 	$values["ns2"] = $ns->NameServer2->HostName;
-    	$values["ns3"] = $ns->NameServer3->HostName;
-    	$values["ns4"] = $ns->NameServer4->HostName;
+	$values["ns3"] = $ns->NameServer3->HostName;
+	$values["ns4"] = $ns->NameServer4->HostName;
 	return $values;
 }
+
 function ascio_SaveNameservers($params) {
 	$ascioParams = mapToOrder($params,"Nameserver_Update");
 	return request("CreateOrder",$ascioParams);
@@ -62,78 +60,15 @@ function ascio_SaveRegistrarLock($params) {
 	$ascioParams = mapToOrder($params,"Change_Locks");
 	$ascioParams->Order->Domain->TransferLock = $lockstatus;
 	return request("CreateOrder",$ascioParams);
-
-
-}
-
-function ascio_GetEmailForwarding($params) {
-	$username = $params["Username"];
-	$password = $params["Password"];
-	$testmode = $params["TestMode"];
-	$tld = $params["tld"];
-	$sld = $params["sld"];
-	# Put your code to get email forwarding here - the result should be an array of prefixes and forward to emails (max 10)
-	foreach ($result AS $value) {
-		$values[$counter]["prefix"] = $value["prefix"];
-		$values[$counter]["forwardto"] = $value["forwardto"];
-	}
-	return $values;
-}
-
-function ascio_SaveEmailForwarding($params) {
-	$username = $params["Username"];
-	$password = $params["Password"];
-	$testmode = $params["TestMode"];
-	$tld = $params["tld"];
-	$sld = $params["sld"];
-	foreach ($params["prefix"] AS $key=>$value) {
-		$forwardarray[$key]["prefix"] =  $params["prefix"][$key];
-		$forwardarray[$key]["forwardto"] =  $params["forwardto"][$key];
-	}
-	# Put your code to save email forwarders here
-}
-
-function ascio_GetDNS($params) {
-    $username = $params["Username"];
-	$password = $params["Password"];
-	$testmode = $params["TestMode"];
-	$tld = $params["tld"];
-	$sld = $params["sld"];
-    # Put your code here to get the current DNS settings - the result should be an array of hostname, record type, and address
-    $hostrecords = array();
-    $hostrecords[] = array( "hostname" => "ns1", "type" => "A", "address" => "192.168.0.1", );
-    $hostrecords[] = array( "hostname" => "ns2", "type" => "A", "address" => "192.168.0.2", );
-	return $hostrecords;
-
-}
-
-function ascio_SaveDNS($params) {
-    $username = $params["Username"];
-	$password = $params["Password"];
-	$testmode = $params["TestMode"];
-	$tld = $params["tld"];
-	$sld = $params["sld"];
-    # Loop through the submitted records
-	foreach ($params["dnsrecords"] AS $key=>$values) {
-		$hostname = $values["hostname"];
-		$type = $values["type"];
-		$address = $values["address"];
-		# Add your code to update the record here
-	}
-    # If error, return the error message in the value below
-	$values["error"] = $Enom->Values["Err1"];
-	return $values;
 }
 
 function ascio_RegisterDomain($params) {
 	$ascioParams = mapToOrder($params,"Register_Domain");
 	$result = request("CreateOrder",$ascioParams);
 	if (!$result) {
-		$command = "updateclientdomain";
-	 	$adminuser = "manuel";
 	 	$values["domain"] =  $params["sld"] ."." . $params["tld"];
 	 	$values["status"] = "Pending";
-	 	localAPI($command,$values,$adminuser);
+	 	callApi("updateclientdomain",$values);
 	}
 	return $result; 
 }
@@ -216,8 +151,8 @@ function ascio_SaveContactDetails($params) {
 }
 
 function ascio_GetEPPCode($params) {
-    $ascioParams = mapToOrder($params,"Update_AuthInfo");
-    // todo: set AuthInfo before order;	
+  $ascioParams = mapToOrder($params,"Update_AuthInfo");
+  // todo: set AuthInfo before order;	
 	$result = request("CreateOrder",$ascioParams,true);
 	if(is_array($result)) {
 		return $result;
@@ -226,39 +161,40 @@ function ascio_GetEPPCode($params) {
 	}
 }
 
+
+function ascio_GetEmailForwarding($params) {
+  $values["error"] = "Operation not allowed";
+  return $values;
+}
+
+function ascio_SaveEmailForwarding($params) {
+  $values["error"] = "Operation not allowed";
+  return $values;
+}
+
+function ascio_GetDNS($params) {
+  $values["error"] = "Operation not allowed";
+  return $values;
+}
+
+function ascio_SaveDNS($params) {
+  $values["error"] = "Operation not allowed";
+  return $values;
+}
+
 function ascio_RegisterNameserver($params) {
-    $username = $params["Username"];
-	$password = $params["Password"];
-	$testmode = $params["TestMode"];
-	$tld = $params["tld"];
-	$sld = $params["sld"];
-    $nameserver = $params["nameserver"];
-    $ipaddress = $params["ipaddress"];
-    # Put your code to register the nameserver here
-    # If error, return the error message in the value below
-    $values["error"] = $error;
-    return $values;
+  $values["error"] = "Operation not allowed";
+  return $values;
 }
 
 function ascio_ModifyNameserver($params) {
-    $username = $params["Username"];
-	$password = $params["Password"];
-	$testmode = $params["TestMode"];
-	$tld = $params["tld"];
-	$sld = $params["sld"];
-    $nameserver = $params["nameserver"];
-    $currentipaddress = $params["currentipaddress"];
-    $newipaddress = $params["newipaddress"];
-    # Put your code to update the nameserver here
-    # If error, return the error message in the value below
-    $values["error"] = $error;
-    //Nameserver_Update
-    return $values;
+  $values["error"] = "Operation not allowed";
+  return $values;
 }
 
 function ascio_DeleteNameserver($params) {
-    $values["error"] = "Operation not allowed";
-    return $values;
+  $values["error"] = "Operation not allowed";
+  return $values;
 }
 
 ?>
