9182affbfef1744f708002d5cf24428c8bef803e
Manuel Lautenschlager <manuel.lautenschlager@ascio.com>
2014-06-20 13:30:06 +0200

    it: auto-fix wrong inputs, set defaults when needed

--- tlds/it/it.php
+++ tlds/it/it.php
@@ -1,5 +1,4 @@
 <?php
-
 class it extends Request {	
 	protected function mapToRegistrant($params) {
 		$contact = parent::mapToRegistrant($params);
@@ -12,9 +11,21 @@ class it extends Request {
 			"other subjects" 						=> "6",
 			"non natural foreigners" 				=> "7"
 		);
-		$contact["RegistrantType"] 		= $map[$params["additionalfields"]["Legal Type"]];
+		// 7 is for all non-italian complanies. Fix invalid user-inputs
+		if(($params["registrantcountry"] != "IT") && $contact["OrgName"]) {
+			$contact["RegistrantType"] = 7;	
+		} 
+		else {
+			$contact["RegistrantType"] 		= $map[$params["additionalfields"]["Legal Type"]];
+		}
 		$contact["RegistrantNumber"]  	= $params["additionalfields"]["Tax ID"];
 		return $contact;
 	}
+	protected function mapToTrademark($params) {		
+		// If the country is non italian, state is not needed. Just set any country. Won't be in the Whois
+		if($params["additionalfields"]["Legal Type"]== "Italian and foreign natural persons") {
+			return array ("Country" => "DK");
+		} 
+	}	
 }
 ?>
