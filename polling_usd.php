162ee878f96396d615ba945c762347935c3cbd38
rendermani <ml@webrender.de>
2015-02-11 16:52:01 +0100

    synced usd scripts

---
+++ polling_usd.php
@@ -0,0 +1,18 @@
+<?
+set_time_limit ( 6000 );
+require_once("../../../init.php");
+require_once "../../../includes/registrarfunctions.php";
+require_once("lib/Request.php");
+
+$pathArr = split("/",$_SERVER['PHP_SELF']);
+$account = $pathArr[count($pathArr)-1] == "polling_usd.php" ? "ascio_usd" : "ascio";
+$cfg = getRegistrarConfigOptions($account);
+$request = new Request($cfg);
+$result = $request->poll();
+while ($result->item && $result->item->MsgId) {
+	$item = $result->item;
+	$request->getCallbackData($item->OrderStatus,$item->MsgId,$item->OrderId);
+	syslog(LOG_INFO,"Acking: ".$result->item->MsgId);
+	$result = $request->poll();
+}
+?>
