f49467644c854286eddea0b6811fcebd65e54dc5
Pedro Frejo <pedro.frejo@arpasystem.com>
2015-03-28 13:46:30 +0100

    ES: Using 'original' array according to mapToOrder()

--- tlds/es/es.php
+++ tlds/es/es.php
@@ -32,7 +32,7 @@ class es extends Request {
 		return $contact;
 	}
 	public function transferDomain($params=false) {
-		$params["regperiod"] = 0 ; 
+		$params["original"]["regperiod"] = 0 ; 
 		parent::transferDomain($params);
 	}
 }
