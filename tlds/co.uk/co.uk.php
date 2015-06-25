de60d25ea59d44cbabbc2a2568c5163af5d3d196
rendermani <ml@webrender.de>
2015-06-25 10:17:07 +0200

    remove leftover code

--- tlds/co.uk/co.uk.php
+++ tlds/co.uk/co.uk.php
@@ -20,11 +20,7 @@ class ok_uk extends Request {
 		$contact["RegistrantType"] 			= $map[$params["additionalfields"]["Legal Type"]];
 		$contact["RegistrantNumber"] 		= $map[$params["additionalfields"]["Company ID Number"]];
 		if(($contact["CountryCode"] != "GB") &! $isCompany) $contact["RegistrantType"] ="FIND";
+		return $contact;
 	}	
 }
-
-$additionaldomainfields[".co.uk"][] = array("Name" => "Legal Type", "LangVar" => "uktldlegaltype", "Type" => "dropdown", "Options" => "Individual,UK Limited Company,UK Public Limited Company,UK Partnership,UK Limited Liability Partnership,Sole Trader,UK Registered Charity,UK Entity (other),Foreign Organization,Other foreign organizations", "Default" => "Individual",);
-$additionaldomainfields[".co.uk"][] = array("Name" => "Company ID Number", "LangVar" => "uktldcompanyid", "Type" => "text", "Size" => "30", "Default" => "", "Required" => false,);
-$additionaldomainfields[".co.uk"][] = array("Name" => "Registrant Name", "LangVar" => "uktldregname", "Type" => "text", "Size" => "30", "Default" => "", "Required" => true,);
-
 ?>
