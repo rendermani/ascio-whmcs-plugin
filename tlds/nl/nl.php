58f862d37af0fc96102e073fc702b69da1f20d8c
Manuel Lautenschlager <manuel.lautenschlager@ascio.com>
2015-07-14 14:13:26 +0200

    new TLD-module for .nl

--- tlds/nl/nl.php
+++ tlds/nl/nl.php
@@ -3,12 +3,32 @@
 class nl extends Request {	
 	protected function mapToRegistrant($params) {
 		$contact = parent::mapToRegistrant($params);
-		if($contact["OrgName"]) {
-			$contact["RegistrantNumber"] = $params["additionalfields"]["Organization Number"];
-			if(!$contact["RegistrantNumber"]) $contact["RegistrantNumber"] = "123123123";
+		$contact["RegistrantNumber"] = $params["additionalfields"]["Organisation Number"];
+		$contact["RegistrantType"] = "PERSOON";	
+		if($contact["OrgName"] && $contact["CountryCode"] == "NL") {
+			$contact["RegistrantType"] = "BV";	
+		} else if($contact["OrgName"]) {
+			$contact["RegistrantType"] = "BGG";	
+		} else {
+			$contact["RegistrantType"] = "PERSOON";	
 		}
+		if($contact["OrgName"] && ($contact["RegistrantNumber"]==""))	throw new AscioException("Please enter a valid Organization Number");
 		return $contact;
 	}
-
+	protected function mapToAdmin($params) {
+		$contact = parent::mapToAdmin($params);
+		$contact["Type"] = "BGG";			
+		return $contact;
+	}
+	protected function mapToTech($params) {
+		$contact = parent::mapToTech($params);
+		$contact["Type"] = "BGG";			
+		return $contact;
+	}
+	protected function mapToBilling($params) {
+		$contact = parent::mapToBilling($params);
+		$contact["Type"] = "BGG";			
+		return $contact;
+	}
 }
 ?>
