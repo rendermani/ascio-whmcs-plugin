8f8e11879e0ff61ed06262b50557870d3befeec4
rendermani <ml@webrender.de>
2017-02-17 15:53:07 +0100

    added .sg

---
+++ tlds/sg/additionaldomainfields.php
@@ -0,0 +1,6 @@
+<?php
+
+$additionaldomainfields[".sg"][] = array("Name" => "Registrant ID", "LangVar" => "registrantid", "Type" => "text", "Description" => "This must be filled out with the Organizations' number 
+If Registrant is an individual, Personal ID number is required.", 'DisplayName' => 'Registrant ID');
+
+?>
