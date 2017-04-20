a3d71cf21dc5326141cdd2c6b62031aab221a740
rendermani <ml@webrender.de>
2017-04-20 14:40:40 +0200

    updated the .FI Tld. RegistrantNumber and Type mapping

--- tlds/fi/fi.php
+++ tlds/fi/fi.php
@@ -1,12 +1,20 @@
 <?php
 
-class nl extends Request {	
+class fi extends Request {	
 	protected function mapToRegistrant($params) {
 		$contact = parent::mapToRegistrant($params);
-		if($contact["OrgName"]) {
-			$contact["RegistrantNumber"] = $params["additionalfields"]["Organization Number"];
-			if(!$contact["RegistrantNumber"]) $contact["RegistrantNumber"] = "123123123";
-		}
+		$map = array(
+			"Individual" 					=> "0",
+			"Company" 						=> "1",
+			"Association" 					=> "2",
+			"Foundation/Institution" 		=> "3",
+			"Political party" 				=> "4",
+			"Municipality/Township" 		=> "5",
+			"State/Government" 				=> "6",
+			"Public corporation/community" 	=> "7"
+		);
+		$contact["RegistrantNumber"] 	= $params["additionalfields"]["Identification Number"];
+		$contact["RegistrantType"] 		= $map[$params["additionalfields"]["Legal Type"]];
 		return $contact;
 	}
 
