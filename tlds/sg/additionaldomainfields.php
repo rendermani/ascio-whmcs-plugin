edd82a1bb667a7848bf7fced7f706634674dd7ed
rendermani <ml@webrender.de>
2017-04-27 09:36:51 +0200

    improved description and added the admin-id

--- tlds/sg/additionaldomainfields.php
+++ tlds/sg/additionaldomainfields.php
@@ -1,6 +1,7 @@
 <?php
 
-$additionaldomainfields[".sg"][] = array("Name" => "Registrant ID", "LangVar" => "registrantid", "Type" => "text", "Description" => "This must be filled out with the Organizations' number 
-If Registrant is an individual, Personal ID number is required.", 'DisplayName' => 'Registrant ID');
+$additionaldomainfields[".sg"][] = array("Name" => "Registrant ID", "LangVar" => "registrantid", "Type" => "text", "Description" => "This is to be filled out with the business registration number (from ROC /RCB certificate) of the registrant", 'DisplayName' => 'Registrant ID');
+
+$additionaldomainfields[".sg"][] = array("Name" => "Admin ID", "LangVar" => "Admin ID", "Type" => "text", "Description" => " Organization number - For the purposes of verification, this field needs to be filled with correct SINGPASS ID [NRIC/FIN], UEN, SGNICID. ", 'DisplayName' => 'Admin ID');
 
 ?>
