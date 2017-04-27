53ea0f2629e8f96c0e2dbcacdb3bf7939da1f3b3
rendermani <ml@webrender.de>
2017-04-27 14:15:24 +0200

    added Local Presence as selectable option

--- tlds/sg/additionaldomainfields.php
+++ tlds/sg/additionaldomainfields.php
@@ -4,4 +4,6 @@ $additionaldomainfields[".sg"][] = array("Name" => "Registrant ID", "LangVar" =>
 
 $additionaldomainfields[".sg"][] = array("Name" => "Admin ID", "LangVar" => "Admin ID", "Type" => "text", "Description" => " Organization number - For the purposes of verification, this field needs to be filled with correct SINGPASS ID [NRIC/FIN], UEN, SGNICID. ", 'DisplayName' => 'Admin ID');
 
+$additionaldomainfields[".sg"][] = array("Name" => "Local Presence", "LangVar" => "Admin ID", "Type" => "tickbox", "Description" => "For adminstrative contacts that are not in singapore", 'DisplayName' => 'Local Presence');
+
 ?>
--- tlds/sg/sg.php
+++ tlds/sg/sg.php
@@ -1,5 +1,16 @@
 <?php
 class sg extends Request {	
+	public function mapToOrder($params,$orderType) {
+		$ascioParams = parent::mapToOrder($params,$orderType);
+		if($params["additionalfields"]["Local Presence"]=="on"){
+			$ascioParams["order"]["LocalPresence"] = "LocalPresenceAdmin";		
+		} else {
+			var_dump($params["additionalfields"]);
+			die();	
+		}
+		
+		return $ascioParams;
+	}
 	protected function mapToRegistrant($params) {
 		$contact = parent::mapToRegistrant($params);
 		$contact["RegistrantNumber"] = $params["additionalfields"]["Registrant ID"];
