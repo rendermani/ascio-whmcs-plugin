94a71d81541823e651e172d8b241c99de33b1262
rendermani <ml@webrender.de>
2017-05-04 11:43:09 +0200

    additional fields added

---
+++ tlds/dk/additionalfields.php
@@ -0,0 +1,13 @@
+<?php
+$additionaldomainfields[".dk"] = array();
+$additionaldomainfields[".dk"][] = array(
+                                                          "Name" => "Registrant CVR nr.",
+                                                          "Type" => "text",
+                                                          "Required" => false,
+                                                          "Description" => "(Skal udfyldes for virksomheder)"                                                       
+);
+$additionaldomainfields[".dk"][] = array(
+                                                          "Name" => "Administrator CVR nr.",
+                                                          "Type" => "text",
+                                                          "Required" => false,
+                                                          "Description" => "(Skal udfyldes for virksomheder)"      
