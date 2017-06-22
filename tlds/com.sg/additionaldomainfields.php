e3231e44271e0bb6a6fb3143a5f7c8db211ba786
rendermani <ml@webrender.de>
2017-06-22 14:26:06 +0200

    new .SG sub-tlds

---
+++ tlds/com.sg/additionaldomainfields.php
@@ -0,0 +1,9 @@
+<?php
+
+$additionaldomainfields[".sg"][] = array("Name" => "Registrant ID", "LangVar" => "registrantid", "Type" => "text", "Description" => "This is to be filled out with the business registration number (from ROC /RCB certificate) of the registrant", 'DisplayName' => 'Registrant ID');
+
+$additionaldomainfields[".sg"][] = array("Name" => "Admin ID", "LangVar" => "Admin ID", "Type" => "text", "Description" => " Organization number - For the purposes of verification, this field needs to be filled with correct SINGPASS ID [NRIC/FIN], UEN, SGNICID. ", 'DisplayName' => 'Admin ID');
+
+$additionaldomainfields[".sg"][] = array("Name" => "Local Presence", "LangVar" => "Admin ID", "Type" => "tickbox", "Description" => "For adminstrative contacts that are not in singapore", 'DisplayName' => 'Local Presence');
+
+?>
---
+++ tlds/org.sg/sg/sg.php
@@ -0,0 +1,22 @@
+<?php
+class sg extends Request {	
+	public function mapToOrder($params,$orderType) {
+		$ascioParams = parent::mapToOrder($params,$orderType);
+		if($params["additionalfields"]["Local Presence"]=="on"){
+			$ascioParams["order"]["LocalPresence"] = "LocalPresenceAdmin";		
+		} 
+		
+		return $ascioParams;
+	}
+	protected function mapToRegistrant($params) {
+		$contact = parent::mapToRegistrant($params);
+		$contact["RegistrantNumber"] = $params["additionalfields"]["Registrant ID"];
+		return $contact;
+	}
+	protected function mapToAdmin($params) {
+		$contact = parent::mapToAdmin($params);
+		$contact["OrganisationNumber"] = $params["additionalfields"]["Admin ID"];
+		return $contact;
+	}
+}
+?>
