6eb7dc27eba6ba9ae773c1ec653c1dbad65090b6
rendermani <ml@webrender.de>
2019-04-11 14:42:50 +0200

    difference .sg <-> *.sg: Registrant Number

--- includes/additionalfields.php
+++ includes/additionalfields.php
@@ -61,10 +61,11 @@ $additionaldomainfields[".sg"][] = array("Name" => "Admin ID", "LangVar" => "Adm
 $additionaldomainfields[".sg"][] = array("Name" => "Local Presence", "LangVar" => "Admin ID", "Type" => "tickbox", "Description" => "For adminstrative contacts that are not in singapore", 'DisplayName' => 'Local Presence');
 
 $additionaldomainfields[".com.sg"] = $additionaldomainfields[".sg"];
-$additionaldomainfields[".edu.sg"] = $additionaldomainfields[".sg"];
-$additionaldomainfields[".net.sg"] = $additionaldomainfields[".sg"];
-$additionaldomainfields[".org.sg"] = $additionaldomainfields[".sg"];
-$additionaldomainfields[".per.sg"] = $additionaldomainfields[".sg"];
+$additionaldomainfields[".com.sg"][] = array("Name" => "Registrant ID", "LangVar" => "registrantid", "Type" => "text", "Description" => "This is to be filled out with the business registration number (from ROC /RCB certificate) of the registrant", 'DisplayName' => 'Registrant ID');
+$additionaldomainfields[".edu.sg"] = $additionaldomainfields[".com.sg"];
+$additionaldomainfields[".net.sg"] = $additionaldomainfields[".com.sg"];
+$additionaldomainfields[".org.sg"] = $additionaldomainfields[".com.sg"];
+$additionaldomainfields[".per.sg"] = $additionaldomainfields[".com.sg"];
 
 // .TEL
 $additionaldomainfields[".tel"] = [];
--- tlds/sg/additionaldomainfields.php
+++ tlds/sg/additionaldomainfields.php
@@ -6,4 +6,11 @@ $additionaldomainfields[".sg"][] = array("Name" => "Registrant ID", "LangVar" =>
 $additionaldomainfields[".sg"][] = array("Name" => "Admin ID", "LangVar" => "Admin ID", "Type" => "text", "Description" => " Organization number - For the purposes of verification, this field needs to be filled with correct SINGPASS ID [NRIC/FIN], UEN, SGNICID. ", 'DisplayName' => 'Admin ID');
 $additionaldomainfields[".sg"][] = array("Name" => "Local Presence", "LangVar" => "Admin ID", "Type" => "tickbox", "Description" => "For adminstrative contacts that are not in singapore", 'DisplayName' => 'Local Presence');
 
+$additionaldomainfields[".com.sg"] = $additionaldomainfields[".sg"];
+$additionaldomainfields[".com.sg"][] = array("Name" => "Registrant ID", "LangVar" => "registrantid", "Type" => "text", "Description" => "This is to be filled out with the business registration number (from ROC /RCB certificate) of the registrant", 'DisplayName' => 'Registrant ID');
+$additionaldomainfields[".edu.sg"] = $additionaldomainfields[".com.sg"];
+$additionaldomainfields[".net.sg"] = $additionaldomainfields[".com.sg"];
+$additionaldomainfields[".org.sg"] = $additionaldomainfields[".com.sg"];
+$additionaldomainfields[".per.sg"] = $additionaldomainfields[".com.sg"];
+
 ?>
