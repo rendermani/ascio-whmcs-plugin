f7d4246b602facc1d4a30923bd340204892cdf8b
rendermani <ml@webrender.de>
2021-07-06 19:20:03 +0200

    updated registry rules .ru

--- tlds/ru/ru.php
+++ tlds/ru/ru.php
@@ -3,11 +3,41 @@
 class ru extends Request {	
 	protected function mapToRegistrant($params) {
 		$contact = parent::mapToRegistrant($params);		
-		if($contact["OrgName"] && $params["additionalfields"]["INN number"] && $params["additionalfields"]["KPP number"]) {
-			$contact["RegistrantNumber"] = $params["additionalfields"]["INN number"];
-			$contact["VatNumber"] = $params["additionalfields"]["KPP number"];
-			$contact["RegistrantType"] = "ORG";			
+		
+		if($params["additionalfields"]["Registrant Type"] == "ORG") {
+			$contact["RegistrantNumber"] = $params["additionalfields"]["Russian Organizations Taxpayer Number 1"];
+			$contact["VatNumber"] = $params["additionalfields"]["Russian Organizations Territory-Linked Taxpayer Number 2"];
+			$contact["RegistrantType"] = "ORG";
+		} else {
+			$contact["RegistrantNumber"] = 	$params["additionalfields"]["Individuals Passport Number"] . ", " . $params["additionalfields"]["Individuals Passport Issue Date"] . ", " . $params["additionalfields"]["Individuals Passport Issuer"];
+			$contact["RegistrantDate"] = $params["additionalfields"]["Individuals Birthday"];
+			$contact["RegistrantType"] = "PRS";	
+			$contact["OrgName"] = $contact["Name"];
 		}
+		$contact["Options"] = $params["additionalfields"]["NIC/D handle"];
+		return $contact;
+	}
+	protected function mapToAdmin($params) {
+		if($params["additionalfields"]["Registrant Type"] == "ORG") { 
+			$company = $params["companyname"];
+		} else {
+			$company = $params["firstname"]. " ".$params["lastname"];
+		}
+		$registrant = $this->mapToRegistrant($params);
+		$contact = Array(
+			'FirstName' 	=> $params["firstname"],
+			'LastName' 		=> $params["lastname"],
+			'OrgName' 		=> $company,
+			'Address1' 		=> $params["address1"],	
+			'Address2' 		=> $params["address2"],
+			'PostalCode' 	=> $params["postcode"],
+			'City' 			=> $params["city"],
+			'State' 		=> $params["state"],		
+			'CountryCode' 	=> $params["country"],	
+			'Email' 		=> $params["email"],
+			'Phone'			=> Tools::fixPhone($params["fullphonenumber"],$country),
+			'Fax' 			=> Tools::fixPhone($params["custom"]["Fax"],$country),
+		);
 		return $contact;
 	}
 
