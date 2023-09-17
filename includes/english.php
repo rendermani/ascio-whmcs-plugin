18bbdd55e34ef2b8650d479c0b4b02be7875d2de
mlautenschlager-tc <111001697+mlautenschlager-tc@users.noreply.github.com>
2023-09-18 00:53:25 +0200

    added more translation

--- includes/english.php
+++ includes/english.php
@@ -2,4 +2,6 @@
 $_LANG["last_from_address"] = "Last from address";
 $_LANG["last_to_address"] = "Last to address";
 $_LANG["last_try_date"] = "Last try date";
-$_LANG["verification_email"] = "Verification email";
+$_LANG["verification_email"] = "Verification Email";
+$_LANG["verified_date"] = "Verification Date";
+$_LANG["verified_by"] = "Verified By";
