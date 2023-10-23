5f3ee932372e2b12d82a80fb782e46cc8d239c77
mlautenschlager-tc <111001697+mlautenschlager-tc@users.noreply.github.com>
2023-10-23 13:11:44 +0200

    new com.my tld plugin

---
+++ tlds/_com.my/com.my.php
@@ -0,0 +1,17 @@
+<?php
+class com_my extends Request {	
+	public function mapToOrder($params,$orderType) {
+		$ascioParams = parent::mapToOrder($params,$orderType);
+		if($params["additionalfields"]["Local Presence"]=="on"){
+			$ascioParams["order"]["LocalPresence"] = "LocalPresenceRegistrant";		
+		} 
+		
+		return $ascioParams;
+	}
+	protected function mapToRegistrant($params) {
+		$contact = parent::mapToRegistrant($params);
+		$contact["RegistrantNumber"] = $params["additionalfields"]["Registrant ID"];
+		return $contact;
+	}
+}
+?>
