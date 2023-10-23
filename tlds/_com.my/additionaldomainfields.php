90bfb18343146e74cd644c1dc9b4ee42800fd0c2
mlautenschlager-tc <111001697+mlautenschlager-tc@users.noreply.github.com>
2023-10-23 13:34:38 +0200

    add additional domain fields for .com.my

---
+++ tlds/_com.my/additionaldomainfields.php
@@ -0,0 +1,6 @@
+<?php
+
+$additionaldomainfields[".com.my"][] = array("Name" => "Registrant ID", "LangVar" => "registrantid", "Type" => "text", "Description" => "This is to be filled out with the business registration number (from ROC /RCB certificate) of the registrant", 'DisplayName' => 'Registrant ID');
+$additionaldomainfields[".com.my"][] = array("Name" => "Local Presence", "LangVar" => "Admin ID", "Type" => "tickbox", "Description" => "For adminstrative contacts that are not in singapore", 'DisplayName' => 'Local Presence');
+
+?>
